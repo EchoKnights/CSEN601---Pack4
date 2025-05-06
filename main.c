66096ccb4df0836c1872d87a817efe2f5c80590b
EchoKnights <alytamerg@gmail.com>
2025-05-06 22:45:08 +0300

    added an incomplete execute function + finished the decode hopefully

--- main.c
+++ main.c
@@ -8,20 +8,22 @@
 int PC = 0;
 
 //arrays
+int REGISTER_FILE[66]; //missing the special registers btw.
+
 int MAIN_MEMORY[MEMORY_SIZE] = {
     //I used chatgpt to generate test instructions for debugging, bas please remember to remove them because i don't want to risk a cheating case.
-    0x412A, // ADDI R1, 10
-    0x4254, // ADDI R2, 20
-    0x0301, // ADD R3, R1
-    0x0302, // ADD R3, R2
-    0x5305, // SUBI R3, 5
-    0x7300, // ST R3, 0
-    0x6400, // LD R4, 0
-    0x4902, // BEQZ R4, 2
-    0x4501, // ADDI R5, 1
-    0x0614, // ADD R6, R4
-    0xA000, // NOP
-    0xB000  // HALT
+    0x412A,
+    0x4254,
+    0x0301,
+    0x0302,
+    0x5305,
+    0x7300,
+    0x6400,
+    0x4902,
+    0x4501,
+    0x0614,
+    0xA000,
+    0xB000
 };
 
 
@@ -36,14 +38,92 @@ int fetch_instruction() {
 
 void decode_instruction(int instruction) {
     int opcode = 0;
-    int operand1 = 0;
-    int operand2 = 0;
+    int r1 = 0;
+    int r2 = 0;
+    int r1data = 0;
+    int r2data = 0;
 
-    opcode = (instruction >> 12);
-    operand1 = ( (unsigned) (instruction >> 6) & 0b000000111111);
-    operand2 = ( (unsigned) instruction & 0b0000000000111111);
+    opcode = ((unsigned) instruction >> 12);
+    r1 = ((instruction >> 6) & 0b000000111111);
+    r2 = (instruction & 0b0000000000111111);
 
-    printf("Decoded instruction: %i %i %i\n", opcode, operand1, operand2);
+    r1data = REGISTER_FILE[r1];
+    r2data = REGISTER_FILE[r2];
+
+    printf("Decoded instruction: %i %i %i\n", opcode, r1, r2);
+}
+
+int sign_extend(int imm) {
+    int sign_bit = (imm >> 5);
+    if (sign_bit = 1) {
+        return imm | 0b11000000;
+    }
+    else{
+        return imm | 0b00000000;
+    }
+}
+
+
+//function is still missing the special flags
+void execute_instruction(int opcode, int r1, int r2, int r1data, int r2data) {
+    int imm = sign_extend(r2);
+
+    switch (opcode) { //the switch case should be updated to reflect the actual opcodes.
+        case 0: //add
+        r1data = r1data + r2data;
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 1: //sub
+        r1data = r1data - r2data;
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 2: //mul
+        r1data = r1data * r2data;
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 3: //load immediate
+        r1data = imm;
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 4: //branch if equal zero
+        if(r1data == 0) {
+            PC = PC + 1 + imm;
+        }
+        break;
+
+        case 5: //and
+        r1data = r1data & r2data;
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 6: //or
+        r1data = r1data | r2data;
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 7: //jump register
+        PC = r1data || r2data;
+        break;
+
+        case 8: //shift left circular - empty for now
+        break;
+
+        case 9: //shift right circular - empty for now
+        break;
+
+        case 10: //load byte
+        r1data = MAIN_MEMORY[imm];
+        REGISTER_FILE[r1] = r1data;
+        break;
+
+        case 11: //store byte
+        MAIN_MEMORY[imm] = r1data;
+        break;
+    }
 }
 
 
